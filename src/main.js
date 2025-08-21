25bd06cbc119caf99bc7d216eda7920ee8cd0de6
maric-urs <ursulet.maric@gmail.com>
2025-08-21 08:18:07 -0400

    Mise à jour du projet correction de AOUT 2025

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style.css';
 import 'element-plus/dist/index.css';
+// Import global d'element-plus
 import ElementPlus from 'element-plus'; 
 import App from './App.vue';
 import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
 
 const app = createApp(App);
 
+// Utilisation globale d'element-plus
 app.use(ElementPlus); 
 app.use(setupCalendar, {})
 app.component('VCalendar', Calendar);
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-}
+  optimizeDeps: {
+    include: ['element-plus']
+  }
 })
